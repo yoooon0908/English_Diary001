0d4c4d3e1a7da1794081039f8b464e366be14ce1
三浦宏予 <yoooon0908@yahoo.co.jp>
2016-02-16 17:18:04 +0800

    Initial Commit

---
+++ EnglishDiary001/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  EnglishDiary001
+//
+//  Created by 三浦宏予 on 2016/02/16.
+//  Copyright © 2016年 Hiroyo Miura. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ EnglishDiary001/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  EnglishDiary001
+//
+//  Created by 三浦宏予 on 2016/02/16.
+//  Copyright © 2016年 Hiroyo Miura. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
